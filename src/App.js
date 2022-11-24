bee47aadc2b6762d7d27419bd33c086e180fda23
Robert Giuga <grir2934@scs.ubbcluj.ro>
2022-11-24 21:14:21 +0200

    added auth form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import AuthContext from "./components/Auth/auth-context";
+import AuthForm from "./components/Auth/AuthForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <AuthForm></AuthForm>
+  )
 }
 
 export default App;
