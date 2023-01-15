9d6e019183ded7f1896f236ab804b6ba552a822f
Robert Giuga <grir2934@scs.ubbcluj.ro>
2023-01-15 10:51:38 +0200

    on-of slide btn

--- src/components/PersonCard/PersonCard.jsx
+++ src/components/PersonCard/PersonCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { AddEventPopup } from './AddEventPopup';
+import {AddEventPopup} from './AddEventPopup';
 import './PersonCard.css';
 
-export const PersonCard = ({ user }) => {
+export const PersonCard = ({user}) => {
     const [isOpen, setIsOpen] = React.useState(false);
+    const [isOn, setIsOn] = React.useState(true);
     const handleEvent = (event) => {
         setIsOpen(true);
     };
@@ -11,6 +12,12 @@ export const PersonCard = ({ user }) => {
     const handleCloseModal = () => {
         setIsOpen(false);
     };
+
+    const handleIsOn=(value)=>{
+        setIsOn(value);
+        console.log(value);
+        //to ad api call to it
+    }
 
     return (
         <div className="PersonCard">
@@ -32,7 +39,14 @@ export const PersonCard = ({ user }) => {
             </div>
             <div className='PersonCard-right'>
                 <button className='AddEvent-button' onClick={handleEvent}>Add Event</button>
-                <AddEventPopup isOpen={isOpen} handleClose={handleCloseModal} />
+                <AddEventPopup isOpen={isOpen} handleClose={handleCloseModal}/>
+                <p>
+                    <b>Auto Events:   </b>
+                    <label className="switch">
+                        <input checked={isOn}  onChange={()=>{handleIsOn(!isOn)}} type="checkbox"></input>
+                        <span className="slider round"></span>
+                    </label>
+                </p>
             </div>
         </div>
     );
